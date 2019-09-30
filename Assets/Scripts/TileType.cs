890e4551085e218c9e3fe78bfe2b31c3994fe90d
Deon-DF <deon_ac@yahoo.com>
2019-09-30 20:04:20 +0300

    Initial commit

---
+++ Assets/Scripts/TileType.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TileType
+{
+    public enum terrain { plains, grassland, badlands, peaks}
+    
+    public string name;
+    public terrain terrain_type;
+    public GameObject tilePrefab;
+
+    public bool isWalkable = false;
+    public float movementCost = 1.0f;
+
+    public void SetTile(terrain type)
+    {
+        this.terrain_type = type;
+       switch (type)
+        {
+            case terrain.plains:
+                this.name = "Plains";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_plains");
+                this.isWalkable = true;
+                this.movementCost = 1.0f;
+                break;
+            case terrain.grassland:
+                this.name = "Grassland";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_grassland");
+                this.isWalkable = true;
+                this.movementCost = 1.0f;
+                break;
+            case terrain.badlands:
+                this.name = "Badlands";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_badlands");
+                this.isWalkable = true;
+                this.movementCost = 1.0f;
+                break;
+            case terrain.peaks:
+                this.name = "Peaks";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_peaks");
+                this.isWalkable = false;
+                this.movementCost = 1.0f;
+                break;
+            default:
+                this.name = "Error!";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_plains");
+                this.isWalkable = true;
+                this.movementCost = 1.0f;
+                break;
+        }
+    }
+
+    public TileType()
+    {
+        this.terrain_type = terrain.plains;
+        this.name = "Plains";
+        this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_plains");
+        this.isWalkable = true;
+        this.movementCost = 1.0f;
+    }
+
+    public TileType(terrain _terrain)
+    {
+        this.terrain_type = _terrain;
+        switch (_terrain)
+        {
+            case terrain.plains:
+                this.name = "Plains";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_plains");
+                this.isWalkable = true;
+                this.movementCost = 1.0f;
+                break;
+            case terrain.grassland:
+                this.name = "Grassland";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_grassland");
+                this.isWalkable = true;
+                this.movementCost = 1.0f;
+                break;
+            case terrain.badlands:
+                this.name = "Badlands";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_badlands");
+                this.isWalkable = true;
+                this.movementCost = 1.0f;
+                break;
+            case terrain.peaks:
+                this.name = "Peaks";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_peaks");
+                this.isWalkable = false;
+                this.movementCost = 1.0f;
+                break;
+            default:
+                this.name = "Error!";
+                this.tilePrefab = Resources.Load<GameObject>("Prefabs/Terrain/tile_plains");
+                this.isWalkable = true;
+                this.movementCost = 1.0f;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/game.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+
+    public static game instance;
+
+    [Header("Map variables")]
+    public GameObject map_tiles_root;
+    public GameObject map_units_root;
+    public string WorldName = "World Map";
+    public TileType[,] terrain_tiles;
+
+    public int mapSizeX = 10;
+    public int mapSizeY = 10;
+
+    [Header("Testing objects")]
+    public GameObject current_unit;
+
+    void GenerateMapData()
+    {
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                terrain_tiles [x,y] = new TileType(TileType.terrain.plains);
+            }
+        }
+
+        SetTile(3, 3, TileType.terrain.peaks);
+        SetTile(3, 4, TileType.terrain.peaks);
+        SetTile(4, 3, TileType.terrain.peaks);
+        SetTile(5, 3, TileType.terrain.peaks);
+        SetTile(6, 3, TileType.terrain.peaks);
+        SetTile(6, 4, TileType.terrain.peaks);
+    }
+
+    void SetTile(int x, int y, TileType.terrain type)
+    {
+        if (terrain_tiles != null)
+        {
+            if ((x >= 0) && (x <= mapSizeX - 1) && (y >= 0) && (y <= mapSizeY))
+            {                
+                terrain_tiles [x,y].SetTile(type);
+            } else {
+                Debug.LogError ("X/Y values for SetTile are outside of map bounds!");
+                Debug.Log("x = " + x + ", y = " + y + ", mapsizeX = " + mapSizeX + ", mapsizeY = " + mapSizeY);
+            }
+        } else { Debug.LogError("The object terrain_tiles is not initialized!");}
+    }
+
+    void GenerateMapVisuals()
+    {
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                GameObject maptile = Instantiate (terrain_tiles[x,y].tilePrefab, new Vector3 (x , y, 0), Quaternion.identity);
+                maptile.GetComponent<mapTile>().coordX = x;
+                maptile.GetComponent<mapTile>().coordY = y;
+                maptile.transform.parent = map_tiles_root.transform;
+            }
+        }
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else {
+            Debug.LogError("Another instance of 'game' class is already running!");
+        }
+    }
+
+    void Start()
+    {
+        terrain_tiles = new TileType[mapSizeX,mapSizeY];
+        GenerateMapData();
+        GenerateMapVisuals();
+    }
+
+}
